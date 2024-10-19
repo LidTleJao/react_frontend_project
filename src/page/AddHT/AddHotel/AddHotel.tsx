113f12d3d4ab705a2e8931bba81477e814e71d52
LidTleJao <praphunpongtke@gmail.com>
2024-10-19 21:04:05 +0700

    save

--- src/page/AddHT/AddHotel/AddHotel.tsx
+++ src/page/AddHT/AddHotel/AddHotel.tsx
@@ -149,9 +149,83 @@ function AddHotelPage() {
                     height: "40px",
                   }}
                 >
+                  <MenuItem value={"กรุงเทพมหานคร"}>กรุงเทพมหานคร</MenuItem>
+                  <MenuItem value={"กระบี่"}>กระบี่</MenuItem>
+                  <MenuItem value={"กาญจนบุรี"}>กาญจนบุรี</MenuItem>
                   <MenuItem value={"กาฬสินธุ์"}>กาฬสินธุ์</MenuItem>
+                  <MenuItem value={"กำแพงเพชร"}>กำแพงเพชร</MenuItem>
                   <MenuItem value={"ขอนแก่น"}>ขอนแก่น</MenuItem>
+                  <MenuItem value={"จันทบุรี"}>จันทบุรี</MenuItem>
+                  <MenuItem value={"ฉะเชิงเทรา"}>ฉะเชิงเทรา</MenuItem>
+                  <MenuItem value={"ชลบุรี"}>ชลบุรี</MenuItem>
+                  <MenuItem value={"ชัยนาท"}>ชัยนาท</MenuItem>
+                  <MenuItem value={"ชัยภูมิ"}>ชัยภูมิ</MenuItem>
+                  <MenuItem value={"ชุมพร"}>ชุมพร</MenuItem>
+                  <MenuItem value={"เชียงราย"}>เชียงราย</MenuItem>
+                  <MenuItem value={"เชียงใหม่"}>เชียงใหม่</MenuItem>
+                  <MenuItem value={"ตรัง"}>ตรัง</MenuItem>
+                  <MenuItem value={"ตราด"}>ตราด</MenuItem>
+                  <MenuItem value={"ตาก"}>ตาก</MenuItem>
+                  <MenuItem value={"นครนายก"}>นครนายก</MenuItem>
+                  <MenuItem value={"นครปฐม"}>นครปฐม</MenuItem>
+                  <MenuItem value={"นครพนม"}>นครพนม</MenuItem>
+                  <MenuItem value={"นครราชสีมา"}>นครราชสีมา</MenuItem>
+                  <MenuItem value={"นครศรีธรรมราช"}>นครศรีธรรมราช</MenuItem>
+                  <MenuItem value={"นครสวรรค์"}>นครสวรรค์</MenuItem>
+                  <MenuItem value={"นนทบุรี"}>นนทบุรี</MenuItem>
+                  <MenuItem value={"นราธิวาส"}>นราธิวาส</MenuItem>
+                  <MenuItem value={"น่าน"}>น่าน</MenuItem>
+                  <MenuItem value={"บึงกาฬ"}>บึงกาฬ</MenuItem>
+                  <MenuItem value={"บุรีรัมย์"}>บุรีรัมย์</MenuItem>
+                  <MenuItem value={"ปทุมธานี"}>ปทุมธานี</MenuItem>
+                  <MenuItem value={"ประจวบคีรีขันธ์"}>ประจวบคีรีขันธ์</MenuItem>
+                  <MenuItem value={"ปราจีนบุรี"}>ปราจีนบุรี</MenuItem>
+                  <MenuItem value={"ปัตตานี"}>ปัตตานี</MenuItem>
+                  <MenuItem value={"พระนครศรีอยุธยา"}>พระนครศรีอยุธยา</MenuItem>
+                  <MenuItem value={"พังงา"}>พังงา</MenuItem>
+                  <MenuItem value={"พัทลุง"}>พัทลุง</MenuItem>
+                  <MenuItem value={"พิจิตร"}>พิจิตร</MenuItem>
+                  <MenuItem value={"พิษณุโลก"}>พิษณุโลก</MenuItem>
+                  <MenuItem value={"เพชรบุรี"}>เพชรบุรี</MenuItem>
+                  <MenuItem value={"เพชรบูรณ์"}>เพชรบูรณ์</MenuItem>
+                  <MenuItem value={"แพร่"}>แพร่</MenuItem>
+                  <MenuItem value={"พะเยา"}>พะเยา</MenuItem>
+                  <MenuItem value={"ภูเก็ต"}>ภูเก็ต</MenuItem>
                   <MenuItem value={"มหาสารคาม"}>มหาสารคาม</MenuItem>
+                  <MenuItem value={"มุกดาหาร"}>มุกดาหาร</MenuItem>
+                  <MenuItem value={"แม่ฮ่องสอน"}>แม่ฮ่องสอน</MenuItem>
+                  <MenuItem value={"ยโสธร"}>ยโสธร</MenuItem>
+                  <MenuItem value={"ยะลา"}>ยะลา</MenuItem>
+                  <MenuItem value={"ร้อยเอ็ด"}>ร้อยเอ็ด</MenuItem>
+                  <MenuItem value={"ระนอง"}>ระนอง</MenuItem>
+                  <MenuItem value={"ระยอง"}>ระยอง</MenuItem>
+                  <MenuItem value={"ราชบุรี"}>ราชบุรี</MenuItem>
+                  <MenuItem value={"ลพบุรี"}>ลพบุรี</MenuItem>
+                  <MenuItem value={"ลำปาง"}>ลำปาง</MenuItem>
+                  <MenuItem value={"ลำพูน"}>ลำพูน</MenuItem>
+                  <MenuItem value={"เลย"}>เลย</MenuItem>
+                  <MenuItem value={"ศรีสะเกษ"}>ศรีสะเกษ</MenuItem>
+                  <MenuItem value={"สกลนคร"}>สกลนคร</MenuItem>
+                  <MenuItem value={"สงขลา"}>สงขลา</MenuItem>
+                  <MenuItem value={"สตูล"}>สตูล</MenuItem>
+                  <MenuItem value={"สมุทรปราการ"}>สมุทรปราการ</MenuItem>
+                  <MenuItem value={"สมุทรสงคราม"}>สมุทรสงคราม</MenuItem>
+                  <MenuItem value={"สมุทรสาคร"}>สมุทรสาคร</MenuItem>
+                  <MenuItem value={"สระแก้ว"}>สระแก้ว</MenuItem>
+                  <MenuItem value={"สระบุรี"}>สระบุรี</MenuItem>
+                  <MenuItem value={"สิงห์บุรี"}>สิงห์บุรี</MenuItem>
+                  <MenuItem value={"สุโขทัย"}>สุโขทัย</MenuItem>
+                  <MenuItem value={"สุพรรณบุรี"}>สุพรรณบุรี</MenuItem>
+                  <MenuItem value={"สุราษฎร์ธานี"}>สุราษฎร์ธานี</MenuItem>
+                  <MenuItem value={"สุรินทร์"}>สุรินทร์</MenuItem>
+                  <MenuItem value={"หนองคาย"}>หนองคาย</MenuItem>
+                  <MenuItem value={"หนองบัวลำภู"}>หนองบัวลำภู</MenuItem>
+                  <MenuItem value={"อ่างทอง"}>อ่างทอง</MenuItem>
+                  <MenuItem value={"อำนาจเจริญ"}>อำนาจเจริญ</MenuItem>
+                  <MenuItem value={"อุดรธานี"}>อุดรธานี</MenuItem>
+                  <MenuItem value={"อุตรดิตถ์"}>อุตรดิตถ์</MenuItem>
+                  <MenuItem value={"อุทัยธานี"}>อุทัยธานี</MenuItem>
+                  <MenuItem value={"อุบลราชธานี"}>อุบลราชธานี</MenuItem>
                 </Select>
               </FormControl>
               <TextField
@@ -228,23 +302,31 @@ function AddHotelPage() {
                       try {
                         setLoad(true);
                         if (hotelName == "") {
-                          window.alert("ชื่อโรงแรมไม่ถูกต้อง โปรดกรอกข้อมูลใหม่");
+                          window.alert(
+                            "ชื่อโรงแรมไม่ถูกต้อง โปรดกรอกข้อมูลใหม่"
+                          );
                         } else {
                           if (province == "") {
-                            window.alert("ข้อมูลจังหวัดไม่ถูกต้อง โปรดกรอกข้อมูลใหม่");
+                            window.alert(
+                              "ข้อมูลจังหวัดไม่ถูกต้อง โปรดกรอกข้อมูลใหม่"
+                            );
                           } else {
                             if (address == "") {
-                              window.alert("ที่อยู่โรงแรมไม่ถูกต้อง โปรดกรอกข้อมูลใหม่");
+                              window.alert(
+                                "ที่อยู่โรงแรมไม่ถูกต้อง โปรดกรอกข้อมูลใหม่"
+                              );
                             } else {
                               if (description == "") {
-                                window.alert("รายละเอียดโรงแรมไม่ถูกต้อง โปรดกรอกข้อมูลใหม่");
+                                window.alert(
+                                  "รายละเอียดโรงแรมไม่ถูกต้อง โปรดกรอกข้อมูลใหม่"
+                                );
                               } else {
                                 navigateToAddHotelP2Page();
                               }
                             }
                           }
                         }
-                        
+
                         setLoad(false);
                       } catch (error) {
                         setLoad(false);
